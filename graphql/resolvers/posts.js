32377f6da3272d816b640b01877e63e965baeceb
subhaneetshrestha <subhaneet@noveltytechnology.com>
2021-01-30 15:15:00 +0545

    added validation for post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -40,6 +40,10 @@ module.exports = {
         }, context) {
             const user = checkAuth(context);
 
+            if(body.trim() === '') {
+                throw new Error('Post body must not be empty!');
+            }
+
             const newPost = new Post({
                 body,
                 user: user.id,
